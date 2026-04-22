7c67099a06a7d88d437cb7ddc20a02bd0e2e2cf4
VishX2 <vishwa.amarajith777@gmail.com>
2026-04-23 02:21:49 +0530

    Configured JAX-RS application with /api/v1 base path

---
+++ src/main/java/uk/ac/westminster/smartcampus/config/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package uk.ac.westminster.smartcampus.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class ApplicationConfig extends Application {
+}
