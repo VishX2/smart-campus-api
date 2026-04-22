ad832741dd567fb88d371428da8574c8ee77e3e9
VishX2 <vishwa.amarajith777@gmail.com>
2026-04-23 03:26:16 +0530

    Updated DataStore to include Sensor storage

--- src/main/java/uk/ac/westminster/smartcampus/storage/DataStore.java
+++ src/main/java/uk/ac/westminster/smartcampus/storage/DataStore.java
@@ -1,13 +1,31 @@
 package uk.ac.westminster.smartcampus.storage;
 
 import uk.ac.westminster.smartcampus.models.Room;
+import uk.ac.westminster.smartcampus.models.Sensor;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * DataStore acts as an in-memory database.
+ * 
+ * IMPORTANT:
+ * - No real database is used (as required by coursework)
+ * - Data is stored using HashMaps
+ * - Data will RESET when server restarts
+ */
 public class DataStore {
 
-    // In-memory storage for rooms
+    /**
+     * Stores all rooms
+     * Key: Room ID (e.g., LIB-301)
+     */
     public static Map<String, Room> rooms = new HashMap<>();
+
+    /**
+     * Stores all sensors
+     * Key: Sensor ID (e.g., TEMP-001)
+     */
+    public static Map<String, Sensor> sensors = new HashMap<>();
 
 }
