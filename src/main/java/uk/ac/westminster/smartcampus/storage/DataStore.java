7b475363691251f66344d602052ec44166883954
VishX2 <vishwa.amarajith777@gmail.com>
2026-04-23 03:40:33 +0530

    Added readings storage mapped per sensor

--- src/main/java/uk/ac/westminster/smartcampus/storage/DataStore.java
+++ src/main/java/uk/ac/westminster/smartcampus/storage/DataStore.java
@@ -2,30 +2,18 @@ package uk.ac.westminster.smartcampus.storage;
 
 import uk.ac.westminster.smartcampus.models.Room;
 import uk.ac.westminster.smartcampus.models.Sensor;
+import uk.ac.westminster.smartcampus.models.SensorReading;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-/**
- * DataStore acts as an in-memory database.
- * 
- * IMPORTANT:
- * - No real database is used (as required by coursework)
- * - Data is stored using HashMaps
- * - Data will RESET when server restarts
- */
 public class DataStore {
 
-    /**
-     * Stores all rooms
-     * Key: Room ID (e.g., LIB-301)
-     */
     public static Map<String, Room> rooms = new HashMap<>();
 
-    /**
-     * Stores all sensors
-     * Key: Sensor ID (e.g., TEMP-001)
-     */
     public static Map<String, Sensor> sensors = new HashMap<>();
 
+    // Key = sensorId, Value = list of readings for that sensor
+    public static Map<String, List<SensorReading>> readings = new HashMap<>();
 }
